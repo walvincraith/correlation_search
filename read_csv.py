03ace2b5e8fc66f6cf93901575523fdf8459af20
Calvin Wraith <walvincraith@gmail.com>
2020-04-16 15:43:06 +0100

    concatenating csvs into one output csv

--- read_csv.py
+++ read_csv.py
@@ -1,3 +1,15 @@
-class csvFile:
-    def __init__(self, input_dir, name):
-        self.name = name
+import pandas as pd
+import glob
+
+path = r'/Users/calvinwraith/Jupyter/Data/NFL/player_passing' # use your path
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+
+frame.to_csv(path + "/combined.csv", index = False)
